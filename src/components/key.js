a577887d1f98b972355cefa47c02743ec2102764
aayushkumarjha2009 <82320742+aayushkumarjha2009@users.noreply.github.com>
2024-02-27 14:22:33 +0530

    Update key.js

--- src/components/key.js
+++ src/components/key.js
@@ -1,3 +1,4 @@
 const key = (process.env.PROD === "true"? "https://chitra-api.vercel.app" : "")
+console.log(key)
 // const key = "https://aayushkumarjha2009.github.io/chitra-api"
 export default key
